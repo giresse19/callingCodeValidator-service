92db7f6ce11932c1a575c4a799af0d56ce5f39b1
giresse.ashu <giresse.ashu@kuehne-nagel.com>
2021-06-11 12:40:43 +0300

    Initialize spring boot project.

---
+++ src/main/java/com/rimiTest/CallCodeAndNumber/CountryCallCodeAndNumberValidatorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rimiTest.CallCodeAndNumber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryCallCodeAndNumberValidatorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountryCallCodeAndNumberValidatorServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rimiTest/CallCodeAndNumber/CallCodeAndNumberEntityValidatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rimiTest.CallCodeAndNumber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallCodeAndNumberEntityValidatorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
